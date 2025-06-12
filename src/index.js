f7b0f2dedf3e405de6f80b975b519616c44e41c2
Shivansh782 <148578113+Shivansh782@users.noreply.github.com>
2025-06-12 12:53:18 +0530

    [SELF LEARNING PAGE]

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
       console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     });
   })
